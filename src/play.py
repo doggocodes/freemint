26ff7bf777874e5fa2ec8119312c1749b9b149d1
Elbert Park <69566231+epcodes@users.noreply.github.com>
2021-01-30 19:59:40 -0800

    basic searches are done

--- src/play.py
+++ src/play.py
@@ -1,3 +1,4 @@
+from eval import *
 import chess
 import chess.svg
 
@@ -5,3 +6,47 @@ from IPython.display import SVG
 
 board = chess.Board()
 SVG(chess.svg.board(board=board,size=400))  
+
+#default depth
+depth = 10
+
+def quiesce( alpha, beta ):
+    stand_pat = eval()
+    if( stand_pat >= beta ):
+        return beta
+    if( alpha < stand_pat ):
+        alpha = stand_pat
+
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            board.push(move)        
+            score = -quiesce( -beta, -alpha )
+            board.pop()
+
+            if( score >= beta ):
+                return beta
+            if( score > alpha ):
+                alpha = score  
+    return alpha
+  
+def select(depth):
+    try:
+        move = chess.polyglot.MemoryMappedReader("bookfish.bin").weighted_choice(board).move()
+        movehistory.append(move)
+        return move
+    except:
+        bestMove = chess.Move.null()
+        bestValue = -99999
+        alpha = -100000
+        beta = 100000
+        for move in board.legal_moves:
+            board.push(move)
+            boardValue = -alphabeta(-beta, -alpha, depth-1)
+            if boardValue > bestValue:
+                bestValue = boardValue;
+                best = move
+            if( boardValue > alpha ):
+                alpha = boardValue
+            board.pop()
+        movehistory.append(best)
+        return best  
