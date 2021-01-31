25d60fd8e3abd35a593a58263e113b6c9b79117c
Elbert Park <69566231+epcodes@users.noreply.github.com>
2021-01-30 20:16:50 -0800

    Update play.py

--- src/play.py
+++ src/play.py
@@ -27,6 +27,22 @@ def quiesce( alpha, beta ):
             if( score > alpha ):
                 alpha = score  
     return alpha
+
+def alphabeta( alpha, beta, depthleft ):
+    bestscore = -9999
+    if( depthleft == 0 ):
+        return quiesce( alpha, beta )
+    for move in board.legal_moves:
+        board.push(move)   
+        score = -alphabeta( -beta, -alpha, depthleft - 1 )
+        board.pop()
+        if( score >= beta ):
+            return score
+        if( score > bestscore ):
+            bestscore = score
+        if( score > alpha ):
+            alpha = score   
+    return bestscore
   
 def select(depth):
     try:
