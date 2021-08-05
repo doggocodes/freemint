6864e0f9057d7b1a5c6cf4cc7152af80f9a070a3
Elbert Park <69566231+epcodes@users.noreply.github.com>
2021-08-04 22:25:44 -0700

    Updates to pawn class (from repl)

--- src/py/eval.py
+++ src/py/eval.py
@@ -1,2 +1,63 @@
 from tables import *
 from aim import *
+def eval_check(): 
+  if piececheck1 = PiecesToCheck[0]:
+    plus = random.choice(pawn1moves)
+    coord = pawn1 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn1]:
+        pawn1 = pawn1 + plus - 1 
+  elif piececheck1 = PiecesToCheck[1]:
+    plus = random.choice(pawn2moves)
+    coord = pawn2 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn2]:
+        pawn2 = pawn2 + plus - 1
+    else:
+        pawn2 = pawn2
+  elif piececheck1 = PiecesToCheck[2]:
+    plus = random.choice(pawn3moves)
+    coord = pawn3 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn3]:
+        pawn3 = pawn3 + plus - 1
+    else:
+      pawn3 = pawn3    
+  elif piececheck1 = PiecesToCheck[3]:
+    plus = random.choice(pawn4moves)
+    coord = pawn4 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn4]:
+        pawn4 = pawn4 + plus - 1
+    else:
+      pawn4 = pawn4        
+  if piececheck1 = PiecesToCheck[4]:
+    plus = random.choice(pawn5moves)
+    coord = pawn5 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn5]:
+        pawn5 = pawn5 + plus - 1 
+  elif piececheck1 = PiecesToCheck[5]:
+    plus = random.choice(pawn6moves)
+    coord = pawn6 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn6]:
+        pawn6 = pawn6 + plus - 1
+    else:
+        pawn6 = pawn6
+  elif piececheck1 = PiecesToCheck[6]:
+    plus = random.choice(pawn7moves)
+    coord = pawn7 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn7]:
+        pawn7 = pawn7 + plus - 1
+    else:
+      pawn3 = pawn3    
+  elif piececheck1 = PiecesToCheck[7]:
+    plus = random.choice(pawn8moves)
+    coord = pawn8 + plus
+    checkcoord = coord - 1
+    if pawnevalWhite[checkcoord] > pawnevalWhite[pawn8]:
+        pawn8 = pawn8 + plus - 1
+    else:
+      pawn8 = pawn8                
